e34d8b3536f8c9b26e8096aa98c604a4c177c0f5
Abdelaziz Hussein <ahussein@bu.edu>
2022-08-03 14:58:42 -0400

    update plotter

--- plotter.py
+++ plotter.py
@@ -7,7 +7,7 @@ import sklearn.metrics as metrics
 from sklearn.metrics import balanced_accuracy_score
 
 
-def score_distribution(true_label,nn_label):
+def score_distribution(true_label,nn_label,training_directory):
     """
     Plots the output score distribution of ghosts and real tracks (training datasets)
     """
@@ -15,17 +15,16 @@ def score_distribution(true_label,nn_label):
     plt.figure()
     plt.xlabel('Score')
     plt.ylabel('Number of events')
-    ghost_mask = true_label== 1
+    
+    ghost_mask = true_label == 1
     true_mask = true_label == 0
     nn_label = nn_label.detach().numpy()
     plt.hist(nn_label[true_mask],histtype="step",range=(0,1),label = 'Real Tracks',density=True)
     plt.hist(nn_label[ghost_mask],histtype="step",range=(0,1),label='Ghost Tracks',density=True)
     plt.legend()
-    plt.savefig('training_score_dist.png')
+    plt.savefig(os.path.join(training_directory,'plots/training_score_dist.png'))
     
-    return
-
-def plot_loss(loss,epochs):
+def plot_loss(loss,epochs,training_directory):
     """
     Plots loss vs epoch
     """
@@ -34,22 +33,21 @@ def plot_loss(loss,epochs):
     plt.xlabel('Epoch Time')
     plt.ylabel('loss (A.U.)')
     plt.plot(x,loss)
-    plt.savefig('loss.png')
+    plt.savefig(os.path.join(training_directory,'plots/loss.png'))
     return
 
-def plot_ROC(y_test,y_pred):
+def plot_ROC(y_test,y_pred,training_directory):
     """
     Plots ROC curve
     """
     y_test = y_test.values
     y_pred = y_pred.detach().numpy()
     fpr, tpr, threshold = metrics.roc_curve(y_test,y_pred)
-    x = np.linspace(0,1,10)
     plt.figure()
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
     plt.plot(fpr,tpr)
-    plt.savefig('roc.png')
+    plt.savefig(os.path.join(training_directory,'plots/roc.png'))
     return
 
     # def plot_features(X_train,true_label):
@@ -67,7 +65,7 @@ def plot_ROC(y_test,y_pred):
 
     # plot_features(dataset,labels)
 
-
+from IPython import embed
 @click.command()
 @click.option('-t','--training-directory',required=True, help="Training directory")
 def plotter(training_directory):
@@ -75,6 +73,17 @@ def plotter(training_directory):
     #load saved data from training
     with open(os.path.join(training_directory,'objs.pkl'), 'rb') as f: 
         EPOCHS,loss,y_train,y_robust,x_val,y_val= pickle.load(f)
-    plot_loss(EPOCHS,loss)
-    score_distribution(y_train,y_robust)
-    plot_ROC(y_train,y_robust)
+
+    #make plots directory
+    if not os.path.exists(os.path.join(training_directory,'plots')):
+        os.makedirs(os.path.join(training_directory,'plots'))
+
+    #make plots
+    plot_loss(loss,EPOCHS,training_directory)
+    score_distribution(y_train,y_robust,training_directory)
+    plot_ROC(y_train,y_robust,training_directory)
+
+
+
+if __name__ == '__main__':
+    plotter()
