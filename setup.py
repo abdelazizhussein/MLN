1de99d8c5fddd86988824b5fbd71315e9e5b9c3a
Abdelaziz Hussein <56006248+abdelazizhussein@users.noreply.github.com>
2022-08-03 16:19:39 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ with open("requirements.txt") as f:
 
 setup(
     name="MLN",
-    author="Abdelaziz Hussein",
-    author_email="abdelh@mit.edu",
+    author="Abdelaziz Hussein,Ouail Kitouni, Niklas Nolte'",
+    author_email="abdelh@mit.edu,kitouni@mit.edu, nnolte@mit.edu",
     description="Tools for Monotonic Lipschitz Network development",
     packages=find_packages(),
     install_requires=requirements,
